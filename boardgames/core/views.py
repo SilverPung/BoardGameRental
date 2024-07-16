3c49a6a67e8d2a0674fc112b3557ab21dfc63163
SilverPung <kristofer.pawel@gmail.com>
2024-07-16 18:07:15 +0200

    added paginator

--- boardgames/core/views.py
+++ boardgames/core/views.py
@@ -20,7 +20,16 @@ def home(request):
     return render(request, 'core/home.html', {'events': events, 'form': form})
 
 @login_required
+@login_required
 def event_detail(request, event_id):
+    action = request.GET.get('action')
+    iterator = int(request.GET.get('iterator', 0))  # Default to 0 if not provided
+
+    if action == 'increment':
+        iterator += 1
+    elif action == 'decrement':
+        iterator -= 1
+
     event = Event.objects.get(id=event_id)
     games = Game.objects.filter(event=event)
     form = SearchForm()
@@ -28,11 +37,13 @@ def event_detail(request, event_id):
         form = SearchForm(request.POST)
         if form.is_valid():
             query = form.cleaned_data['query']
-            # Filter by title or barcode
             games = Game.objects.filter(
                 Q(title__icontains=query) | Q(barcode__icontains=query),
-                event=event  # Ensure to also filter by event
+                event=event
             )
-    top_games = games.order_by('title')[:5]
-    return render(request, 'core/event_detail.html', {'event': event, 'form': form, 'top_games': top_games})
-
+    # Calculate the slice indices based on the iterator
+    start_index = iterator * 5
+    end_index = start_index + 5
+    top_games = games.order_by('title')[start_index:end_index]
+    max_iterator = games.count() // 5
+    return render(request, 'core/event_detail.html', {'event': event, 'form': form, 'top_games': top_games, 'iterator': iterator, 'max_iterator': max_iterator})
