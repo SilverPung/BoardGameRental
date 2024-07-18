f2a7114899357760a9b730ff75af73c999e7cf63
SilverPung <kristofer.pawel@gmail.com>
2024-07-18 19:54:04 +0200

    added pop up , and fixed some mistakes

--- boardgames/games/views.py
+++ boardgames/games/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import GameForm, RentalForm
 from core.models import Game, Event, Renter
+from django.contrib import messages
 # Create your views here.
 LIMIT_OF_RENTED_GAMES = 2 #limit of games that can be rented by one user
 
@@ -24,15 +25,20 @@ def edit_game(request, game_id):
     form = GameForm(instance=game)  
     rental_form = RentalForm()  
     list_of_renters = game.list_of_renters.all()  
-
     if request.method == 'POST':
         form_type = request.POST.get('form_type', None)
+        
         if form_type == 'rental':#renting game
             rental_form = RentalForm(request.POST)
+
             if rental_form.is_valid():
                 renters_barcode=rental_form.cleaned_data['barcode']
-                if renters_barcode in list_of_renters.values_list('barcode', flat=True) or Renter.objects.get(barcode=renters_barcode).how_many_games>=LIMIT_OF_RENTED_GAMES:
-                    pass
+                try:
+                    can_be_rented = Renter.objects.get(barcode=renters_barcode).how_many_games>=LIMIT_OF_RENTED_GAMES
+                except Renter.DoesNotExist:
+                    can_be_rented = False
+                if renters_barcode in list_of_renters.values_list('barcode', flat=True) or can_be_rented:
+                    messages.error(request, 'Renter has reached the limit of rented games or has allready rented this game.')
                 else:
                     game.add_renter(renters_barcode)
                     game.accessible -= 1
@@ -40,16 +46,22 @@ def edit_game(request, game_id):
                     renter = Renter.objects.get(barcode=renters_barcode)
                     renter.how_many_games+=1
                     renter.save()
+                    messages.success(request, 'Renter was successfully added.')
+                
                 return redirect('core:event_detail', game.event.id)
+            
             else:
                 print(rental_form.errors)
+
         elif form_type == 'edit_game':  # editing game
             form = GameForm(request.POST, instance=game)
             if form.is_valid():
                 form.save()
+                messages.success(request, 'Game was successfully edited.')
                 return redirect('core:event_detail', game.event.id)
             else:
                 print(form.errors)
+
         if 'renter_id' in request.POST:#removing renter
             renter_id = request.POST.get('renter_id', None)
             renter = game.list_of_renters.get(id=renter_id)
@@ -58,6 +70,7 @@ def edit_game(request, game_id):
             game.list_of_renters.remove(renter)
             game.accessible += 1
             game.save()
+            messages.success(request, 'Renter was successfully removed.')
             return redirect('core:event_detail', game.event.id)
 
     return render(request, 'games/edit_game.html', {'form': form, 'game': game, 'rental_form': rental_form, 'list_of_renters': list_of_renters})
